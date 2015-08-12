b110da83b81b5997bf08d4d2f1f56fc94150cc83
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-12 13:53:48 -0400

    test error handling

--- pyresolve_tests.py
+++ pyresolve_tests.py
@@ -9,6 +9,7 @@ class ResolveTests(unittest.TestCase):
         self.assertEqual(len(header.header), 12)
 
     def testheaderzeros(self):
+        "check entries which should be 0"
         header = Header(random.choice(range(65535)))
         headlist = list(map(int, header.header))
         self.assertEqual(headlist[3], 0)
@@ -16,6 +17,12 @@ class ResolveTests(unittest.TestCase):
         self.assertEqual(headlist[9], 0)
         self.assertEqual(headlist[10], 0)
         self.assertEqual(headlist[11], 0)
+
+    def testbadqnum(self):
+        "number out of valid range"
+        self.assertRaises(ValueError, Header, -1)
+        self.assertRaises(ValueError, Header, 65535)
+        self.assertRaises(ValueError, Header, -65535)
 
 
 if __name__ == '__main__':

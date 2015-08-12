9faaf16d9244c6e5a23fdb2e0d5c0fea9f9764dc
Alice Pote <alice.writes.wrongs@gmail.com>
2015-08-12 13:49:16 -0400

    moar test!

--- pyresolve_tests.py
+++ pyresolve_tests.py
@@ -8,6 +8,15 @@ class ResolveTests(unittest.TestCase):
         header = Header(random.choice(range(65535)))
         self.assertEqual(len(header.header), 12)
 
+    def testheaderzeros(self):
+        header = Header(random.choice(range(65535)))
+        headlist = list(map(int, header.header))
+        self.assertEqual(headlist[3], 0)
+        self.assertEqual(headlist[8], 0)
+        self.assertEqual(headlist[9], 0)
+        self.assertEqual(headlist[10], 0)
+        self.assertEqual(headlist[11], 0)
+
 
 if __name__ == '__main__':
     unittest.main()
